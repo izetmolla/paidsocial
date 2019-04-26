0362de68fa2a9891e263803c365a196b09e0e099
izetmolla <49998885+izetmolla@users.noreply.github.com>
2019-04-26 14:36:10 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "cwzqdvxo_chicas";
-$password = "Endrisa1996";
-$dbname = "cwzqdvxo_chicas";
+$username = "dbusername";
+$password = "dbpassword";
+$dbname = "dbname";
 
 // Create connection
 $link = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($link->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $link->connect_error);
 } 
 
 $mediaServer = 'https://s3.us-east-2.amazonaws.com/PaidSocial/';
